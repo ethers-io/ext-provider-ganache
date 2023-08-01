1fa95ab794f626c4531b8340c4f322e34126ebb2
Richard Moore <github@ricmoo.com>
2023-08-01 18:34:30 -0400

    More manual testing.

--- src.ts/test.ts
+++ src.ts/test.ts
@@ -1,14 +1,32 @@
+import { ethers } from "ethers";
+import ganache from "ganache";
+
 import { GanacheProvider } from "./index.js";
 
-(async function() {
-    const provider = new GanacheProvider();
-    console.log(provider);
-    console.log(await provider.getBlockNumber());
+async function test(provider: GanacheProvider): Promise<void> {
+    console.log("PROVIDER", provider);
+    console.log("BLOCK NUMBER", await provider.getBlockNumber());
 
     const signer = await provider.getSigner();
-    console.log(signer);
+    console.log("SIGNER", signer);
 
-    const tx = await signer.sendTransaction({ to: signer, value: 123 });
-    console.log(tx);
-    console.log(await tx.wait());
+    const wallet = new ethers.Wallet(ethers.id("foo"), provider);
+    console.log("WALLET", wallet);
+
+    {
+        const tx = await signer.sendTransaction({ to: wallet, value: "123000000000000" });
+        console.log("TX1", tx);
+        console.log("RECEIPT1", await tx.wait());
+    }
+
+    {
+        const tx = await wallet.sendTransaction({ to: signer, value: 123 });
+        console.log("TX2", tx);
+        console.log("RECEIPT2", await tx.wait());
+    }
+}
+
+(async function() {
+    await test(new GanacheProvider(ganache.provider({ chain: { chainId: 1336 } })));
+    await test(new GanacheProvider({ chain: { chainId: 13370 } }));
 })();
